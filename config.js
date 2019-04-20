329484cb844eb6beda81484f9cceded43d5feb2c
cosminrepo <37581531+cosminrepo@users.noreply.github.com>
2019-04-20 14:02:58 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-/*
- * Example module for exporting constants to use in multiple files.
- * (It is not required to have a config.js file -- this acts just like any other module)
- */
-
-module.exports = {
-    FOO: 42,
-    GROUP_NAME: "Subscribers"
-};
